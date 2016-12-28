c174d293ff2e9c889d1cb9217ab569d966603550
Daniel <srdaniel83@gmail.com>
2016-12-28 15:37:13 +0100

    Utilización de Prioridades. Con este commit se termina la Gestión de Hilos

---
+++ src/UtilizacionPrioridades/CounterThread.java
@@ -0,0 +1,40 @@
+package UtilizacionPrioridades;
+
+public class CounterThread extends Thread {
+	String name;
+
+	public CounterThread(String name) {
+		this.name = name;
+	}
+
+	public void run() {
+		int count = 0;
+		try {
+			while (++count < 10000) {
+				System.out.println(name + ": " + count);
+				sleep(10);
+			}
+		} catch (InterruptedException e) {
+			System.err.println(name + " interrumpido");
+		}
+	}
+}
+
+class Prioridad {
+	public static void main(String[] args) {
+		CounterThread thread1 = new CounterThread("thread1");
+		//thread1.setPriority(1); //(1) es la mínima prioridad y (10) la máxima
+		thread1.setPriority(Thread.MIN_PRIORITY);
+		CounterThread thread2 = new CounterThread("thread2");
+		thread2.setPriority(Thread.MAX_PRIORITY);		
+		thread2.start();
+		thread1.start();		
+		try {
+			Thread.sleep(1000);
+			thread1.interrupt();
+			thread2.interrupt();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
